e1683bdc90b62d9d833c7a775330d479d44eb0a1
Егор Романченко <egor.gmag@yandex.ru>
2022-03-12 12:25:21 +0700

    zad17_statgrad solved

--- zad17_statgrad/zad17_statgrad.py
+++ zad17_statgrad/zad17_statgrad.py
@@ -1,4 +1,18 @@
 f = open('17.txt')
+a = []
+summ = 0
+counter = 0
+parcount = 0
+parmax = 0
 for i in f:
-    print(i)
-    exit()
+    a.append(int(i))
+for i in a:
+    if i % 2 == 0:
+        summ += i
+        counter += 1
+sred = summ / counter
+for i in range(0,len(a)-1):
+    if a[i]%3==0 and a[i+1]<sred or a[i+1]%3==0 and a[i]<sred:
+        parcount+=1
+        parmax = max(parmax,a[i]+a[i+1])
+print(parcount,parmax)
