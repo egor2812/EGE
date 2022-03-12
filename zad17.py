1616e8050e64fbcca502ce42b267c1678bd46d97
Егор Романченко <egor.gmag@yandex.ru>
2022-03-12 11:19:21 +0700

    zad17 solved zad17_statgrad added, not solved

--- zad17.py
+++ zad17.py
@@ -1,7 +1,83 @@
-count = 0
-maxc = 0
-for i in range(1016, 7938):
-    if i % 3 == 0 and i % 70 != 0 and i % 17 != 0 and i % 19 != 0 and i % 27 != 0:
-        count += 1
-        maxc = max(i, maxc)
-print(count, maxc)
+
+def zad1(count, maxc):
+    for i in range(1016, 7938):
+        if i % 3 == 0 and i % 70 != 0 and i % 17 != 0 and i % 19 != 0 and i % 27 != 0:
+            count += 1
+            maxc = max(i, maxc)
+    return count, maxc
+
+
+def zad2(count, maxc):
+    for i in range(1033, 7738):
+        if i % 5 == 0 and i % 11 != 0 and i % 17 != 0 and i % 19 != 0 and i % 23 != 0:
+            count += 1
+            maxc = max(i, maxc)
+    return count, maxc
+
+
+def zad3(count, maxc):
+    for i in range(1012, 9639):
+        if i % 3 == 0 and i % 11 != 0 and i % 13 != 0 and i % 17 != 0 and i % 19 != 0:
+            count += 1
+            maxc = max(i, maxc)
+    return count, maxc
+
+
+def zad4(count, maxc):
+    for i in range(3201, 12877):
+        if i % 4 == 0 and i % 7 != 0 and i % 11 != 0 and i % 13 != 0 and i % 19 != 0:
+            count += 1
+            maxc = max(i, maxc)
+    return count, maxc
+
+
+def zad5(count, maxc):
+    for i in range(1100, 11001):
+        if i % 6 == 0 and i % 7 != 0 and i % 13 != 0 and i % 17 != 0 and i % 23 != 0:
+            count += 1
+            maxc = max(i, maxc)
+    return count, maxc
+
+
+def zad6(count, maxc):
+    for i in range(1512, 13203):
+        if i % 7 == 0 and i % 11 != 0 and i % 13 != 0 and i % 17 != 0 and i % 23 != 0:
+            count += 1
+            maxc = max(i, maxc)
+    return count, maxc
+
+
+def zad7(count, maxc):
+    for i in range(1606, 9681):
+        if i % 11 == 0 and i % 7 != 0 and i % 13 != 0 and i % 17 != 0 and i % 19 != 0:
+            count += 1
+            maxc = max(i, maxc)
+    return count, maxc
+
+
+def zad8(count):
+    for i in range(1200, 11201):
+        if i % 5 == 0 and i % 7 != 0 and i % 13 != 0 and i % 17 != 0 and i % 19 != 0:
+            count += 1
+            if(count==1): maxc = i
+    return count, maxc
+
+
+def zad9(count):
+    for i in range(200, 9121):
+        if i % 8 == 0 and i % 7 != 0 and i % 11 != 0 and i % 17 != 0 and i % 19 != 0:
+            count += 1
+            if(count==1): maxc = i
+    return count, maxc
+
+
+def zad10(count):
+    for i in range(1107, 9505):
+        if i % 9 == 0 and i % 7 != 0 and i % 15 != 0 and i % 17 != 0 and i % 19 != 0:
+            count += 1
+            if(count==1): maxc = i
+    return count, maxc
+
+
+print(zad10(0))
+
---
+++ zad17_statgrad/zad17_statgrad.py
@@ -0,0 +1,4 @@
+f = open('17.txt')
+for i in f:
+    print(i)
+    exit()
